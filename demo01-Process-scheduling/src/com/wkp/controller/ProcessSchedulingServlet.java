83b8d002a40481dc67b2fc2bd0cb410e3e498f19
wang-kaopu <Wang-kaopu@outlook.com>
2025-04-29 16:29:07 +0800

    fix:修复了ProcessSchedulingServlet中RoundRobinScheduling方法的算法错误

--- demo01-Process-scheduling/src/com/wkp/controller/ProcessSchedulingServlet.java
+++ demo01-Process-scheduling/src/com/wkp/controller/ProcessSchedulingServlet.java
@@ -1,6 +1,7 @@
 package com.wkp.controller;
 
 import com.wkp.dao.impl.PcbDAOImpl;
+import com.wkp.pojo.Jcb;
 import com.wkp.pojo.Pcb;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,6 +12,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
@@ -20,7 +22,7 @@ public class ProcessSchedulingServlet extends HttpServlet {
     private PcbDAOImpl pcbDAO = new PcbDAOImpl();
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp){
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
         String algorithm = req.getParameter("algorithm");
         Class<? extends ProcessSchedulingServlet> clazz = this.getClass();
         Method method;
@@ -35,9 +37,10 @@ public class ProcessSchedulingServlet extends HttpServlet {
 
     private void RoundRobinScheduling(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html;charset=utf-8");
+        PrintWriter respWriter = resp.getWriter();
         // 1. 获取全部进程，按时间先后顺序排序
-        List<Pcb> pcbList = pcbDAO.getAllPcb();
-        pcbList.sort(new Comparator<Pcb>() {
+        LinkedList<Pcb> pcbLinkedList = new LinkedList<>(pcbDAO.getAllPcb());
+        pcbLinkedList.sort(new Comparator<Pcb>() {
             @Override
             public int compare(Pcb o1, Pcb o2) {
                 return o1.getArriveTime().compareTo(o2.getArriveTime());
@@ -45,48 +48,56 @@ public class ProcessSchedulingServlet extends HttpServlet {
         });
         // 2. 获取时间片长度（分钟）
         int timeQuantum = Integer.parseInt(req.getParameter("timeQuantum"));
-        // 3. 将进程列表转为队列
-        LinkedList<Pcb> pcbLinkedList = new LinkedList<>(pcbList);
-        // 4. 开始轮转，一边进行一边输出
-        PrintWriter respWriter = resp.getWriter();
-        int count = 1;
-        int run;
+        // 3. 开始轮转，一边进行一边输出
+        LocalDateTime currentTime = pcbLinkedList.getFirst().getArriveTime();
+        int count = 1, run = 0;
         while (!pcbLinkedList.isEmpty()) {
-            // 出队
-            Pcb head = pcbLinkedList.removeFirst();
-            // 获取两个时间：使用时间和需要运行时间
-            int usedTime = head.getUsedTime();
-            int runTime = head.getRunTime();
-            if (usedTime >= runTime) {
-                continue;
+            String str = "";
+            int pid;
+            currentTime = currentTime.plusMinutes(timeQuantum);
+            Pcb head = null;
+            if (!pcbLinkedList.getFirst().getArriveTime().isAfter(currentTime)) {
+                head = pcbLinkedList.removeFirst();
+                // 获取两个时间：使用时间和需要运行时间
+                int usedTime = head.getUsedTime();
+                int runTime = head.getRunTime();
+                if (usedTime >= runTime) {
+                    continue;
+                }
+                // 使用时间小于需要运行时间
+                int sub = runTime - usedTime;
+                if (sub > timeQuantum) {
+                    // 一个时间片不够用
+                    run = timeQuantum;
+                    ProcessRun(head, respWriter, currentTime, run, "PREPARED");
+                    head.setUsedTime(usedTime + timeQuantum);
+                    pcbLinkedList.addLast(head);
+                    currentTime.plusMinutes(timeQuantum);
+                } else {
+                    // 一个时间片够用
+                    run = sub;
+                    ProcessRun(head, respWriter, currentTime, run, "FINISHED");
+                    head.setUsedTime(runTime);
+                    currentTime.plusMinutes(sub);
+                }
             }
-            // 将状态设为1：busy运行
-            head.setState(1);
-            // 使用时间小于需要运行时间
-            int sub = runTime - usedTime;
-            String str;
-            if (sub > timeQuantum) {
-                // 一个时间片不够用
-                head.setUsedTime(usedTime + timeQuantum);
-                pcbLinkedList.addLast(head);
-                run = timeQuantum;
-                // 将状态设为0：prepared就绪
-                head.setState(0);
-                str = "PREPARED";
-            } else {
-                // 一个时间片够用
-                head.setUsedTime(runTime);
-                run = sub;
-                // 将状态设为2：finished完成
-                head.setState(2);
-                str = "FINISHED";
-            }
-            respWriter.println("第" + count + "个时间片过去了,&emsp;" +
-                    head.getPid() + ":" + head.getProcessName() + "运行了" + run + "分钟，现在其状态为：" +
-                    str + "<br>");
             ++count;
         }
         respWriter.println("轮转完成");
+    }
+
+    /**
+     * 向客户端输出进程运行情况
+     *
+     * @param pcb         进程控制块
+     * @param respWriter  客户端输出流
+     * @param currentTime 当前时间
+     */
+    private void ProcessRun(Pcb pcb, PrintWriter respWriter, LocalDateTime currentTime, int run, String state) {
+        respWriter.println("开始调用进程->时间：" + currentTime +
+                ", 进程" + pcb.getPid() + ":" + pcb.getProcessName() + "开始，状态为：" + "RUNNING" + "<br>");
+        respWriter.println("结束调用进程->时间：" + currentTime.plusMinutes(run) +
+                ", 进程" + pcb.getPid() + ":" + pcb.getProcessName() + "结束，状态为：" + state + "<br><br>");
     }
 
     private void FCFS(HttpServletRequest req, HttpServletResponse resp) {
@@ -133,7 +144,7 @@ public class ProcessSchedulingServlet extends HttpServlet {
                 timeQuantum2 = Integer.parseInt(req.getParameter("timeQuantum2")),
                 timeQuantum3 = Integer.parseInt(req.getParameter("timeQuantum3"));
 
-        try (PrintWriter writer = resp.getWriter()){
+        try (PrintWriter writer = resp.getWriter()) {
             resp.setContentType("text/html");
             detect(queue1, queue2, timeQuantum1, writer);
             detect(queue2, queue3, timeQuantum2, writer);
